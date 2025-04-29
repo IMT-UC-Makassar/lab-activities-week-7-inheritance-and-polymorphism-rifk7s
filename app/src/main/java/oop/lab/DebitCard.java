c5a09531a520484229e0db755931b8f9b27eb497
RIFKY <rifkip1337@gmail.com>
2025-04-29 17:54:57 +0800

    DebitCard class ; adding JavaDoc comments

--- app/src/main/java/oop/lab/DebitCard.java
+++ app/src/main/java/oop/lab/DebitCard.java
@@ -1,11 +1,18 @@
 package oop.lab;
 
+/**
+ * Debit card linked to a bank account with no interest
+ */
 public class DebitCard extends Card {
     
     public DebitCard(String cardNumber, String cardHolder) {
         super(cardNumber, cardHolder);
     }
     
+    /**
+     * Debit cards have no interest
+     * @return 0.0 as interest rate
+     */
     @Override
     public double getInterestRate() {
         return 0.0; // Debit cards have no interest
