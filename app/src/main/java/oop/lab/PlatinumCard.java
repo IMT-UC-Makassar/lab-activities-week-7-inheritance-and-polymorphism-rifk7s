7b8bb69599abbef3e174b603c077d41e57e64a2d
RIFKY <rifkip1337@gmail.com>
2025-04-29 17:56:04 +0800

    PlatinumCard class ; adding JavaDoc comments

--- app/src/main/java/oop/lab/PlatinumCard.java
+++ app/src/main/java/oop/lab/PlatinumCard.java
@@ -1,11 +1,18 @@
 package oop.lab;
 
+/**
+ * Premium platinum credit card with reduced interest rate
+ */
 public class PlatinumCard extends CreditCard {
     
     public PlatinumCard(String cardNumber, String cardHolder) {
         super(cardNumber, cardHolder);
     }
     
+    /**
+     * Returns the premium platinum card interest rate
+     * @return 10.0% interest rate (lower than standard)
+     */
     @Override
     public double getInterestRate() {
         return 10.0; // 10% interest rate for platinum cards (lower than standard)
