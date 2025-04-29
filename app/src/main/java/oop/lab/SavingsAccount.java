76267d687017e9da8a759592f537d115b80482d3
RIFKY <rifkip1337@gmail.com>
2025-04-29 17:56:34 +0800

    SavingsAccount class ; adding JavaDoc comments

--- app/src/main/java/oop/lab/SavingsAccount.java
+++ app/src/main/java/oop/lab/SavingsAccount.java
@@ -1,5 +1,8 @@
 package oop.lab;
 
+/**
+ * Savings account with interest accumulation capabilities
+ */
 public class SavingsAccount extends BankAccount implements OnlineService {
     private static final double INTEREST_RATE = 0.05; // 5% interest
     
@@ -7,18 +10,30 @@ public class SavingsAccount extends BankAccount implements OnlineService {
         super(accountNumber, accountHolder, balance);
     }
     
+    /**
+     * Calculates and applies 5% interest to the account balance
+     */
     @Override
     public void calculateInterest() {
         double interest = getBalance() * INTEREST_RATE;
         setBalance(getBalance() + interest);
     }
     
+    /**
+     * Transfers funds to another account
+     * @param amount amount to transfer
+     * @param destinationAccount destination account number
+     */
     @Override
     public void transferFunds(double amount, String destinationAccount) {
         withdraw(amount);
         // Let's say we would transfer to the destination account
     }
     
+    /**
+     * Pays bills from this account
+     * @param amount amount to pay
+     */
     @Override
     public void payBills(double amount) {
         withdraw(amount);
