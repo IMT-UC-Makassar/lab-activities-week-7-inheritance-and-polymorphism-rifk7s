fa21fca092f04f4e712c82bd9b37944ac9b7cfdd
RIFKY <rifkip1337@gmail.com>
2025-04-29 17:54:21 +0800

    CreditCard class ; adding JavaDoc comment

--- app/src/main/java/oop/lab/CreditCard.java
+++ app/src/main/java/oop/lab/CreditCard.java
@@ -1,11 +1,18 @@
 package oop.lab;
 
+/**
+ * Standard credit card with 15% interest rate
+ */
 public class CreditCard extends Card {
     
     public CreditCard(String cardNumber, String cardHolder) {
         super(cardNumber, cardHolder);
     }
     
+    /**
+     * Returns the standard credit card interest rate
+     * @return 15.0% interest rate
+     */
     @Override
     public double getInterestRate() {
         return 15.0; // 15% interest rate for standard credit cards
